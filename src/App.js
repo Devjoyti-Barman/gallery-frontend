29fd63c2a2d3e65e5eccfce053ff9fcb372722ef
unknown <devjoytib@gmail.com>
2021-10-20 04:58:49 +0530

    instead of sending the image uft8 buffer  we are sending the image encoding base64

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import './App.css';
 import Card from './components/card/card';
+import axios from 'axios';
+import {useEffect, useState} from 'react';
+import logo from './logo512.png';
 
 function App() {
+  
+  const [photo,setPhoto]=useState(null);
+
+  useEffect( () => {
+    async function getImage(){
+      try {
+        const id="616ef43d69a06e93713efff7";
+        const {data}=await axios.get(`http://localhost:5000/api/get-image/${id}`);
+        setPhoto(data);
+      } catch (error) {
+        console.log(error);
+      }
+
+    }
+    
+
+    getImage();
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
           <Card/>
+          {photo===null ? <div></div> : <img src={photo.data}/> }
       </header>
     </div>
   );
