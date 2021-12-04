bad6d69212dea276a7aa8a2d414e16d85dc4ed2f
unknown <devjoytib@gmail.com>
2021-12-04 19:30:50 +0530

    created signup component

--- src/components/signup/signup.js
+++ src/components/signup/signup.js
@@ -1,5 +1,6 @@
 import './signup.css';
-
+import {Link} from 'react-router-dom';
+import { Button,Typography } from '@material-ui/core';
 import googleicon from '../../images/google-icon.png';
 import githubicon from '../../images/github-icon.png';
 
@@ -8,25 +9,64 @@ function Signup(){
     return(
         <div>
             <div className='signup-container'>
-                <div className='signup-btn'>
-                    <div className='signup'>
-                        <img 
-                            src={googleicon}
-                            className='signup-icon' 
-                        />
-                        <div className='signup-text'> Sign in with google</div>
+                <div className='signup'>
+                    <h2>Create an Account</h2>
+                    <p>Sign up with your social media account or email address</p>
+                    <div className='social-btn'>
+                        
+                        <Link to='/' className='social-link'>
+                          <img className='social-img' src={googleicon} />
+                          <div className='social-title'  > Google </div>
+                        </Link>
+
+                        <Link to='/' className='social-link'>
+                          <img className='social-img' src={githubicon} />
+                          <div  className='social-title' > Github </div>
+                        </Link>
+
                     </div>
-                </div>
-                <div className='signup-btn'>
-                    <div className='signup'>
-                        <img 
-                            src={githubicon}
-                            className='signup-icon' 
-                        />
-                        <div className='signup-text'> Sign in with github</div>
+                    {/* google sign up Container */}
+                    <div className='or-seperator'>
+                        <b>or</b>
                     </div>
+                    <div className='signup-group'>
+                        <input 
+                           className='signup-input'
+                           placeholder='Enter your Fullname'
+                           type='text'
+                        />
+                    </div>
+                    <div className='signup-group'>
+                        <input 
+                           className='signup-input'
+                           placeholder='Enter your Email Address'
+                           type='email'
+                        />
+                    </div>
+                    <div className='signup-group'>
+                        <input 
+                           className='signup-input'
+                           placeholder='Enter your Password'
+                           type='password'
+                        />
+                    </div>
+                    <div className='signup-group'>
+                        <input 
+                           className='signup-input'
+                           placeholder='Confirm Password'
+                           type='password'
+                        />
+                    </div>
+                    <Typography align='center'>
+                        <Button 
+                           className='signup-button'
+                           variant="contained" 
+                           color="primary"
+                        >Sign Up
+                        </Button>
+                    </Typography>
                 </div>
-            </div>
+           </div>
         </div>
     )
 }
