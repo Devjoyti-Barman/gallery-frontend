91fc1a8d5c9837dc8c59aabc894c812045d95517
unknown <devjoytib@gmail.com>
2021-12-29 14:14:16 +0530

    made some changes in css of showBlog component

--- src/components/showBlog/showBlog.js
+++ src/components/showBlog/showBlog.js
@@ -4,6 +4,8 @@ import axios from "axios";
 import { Markup } from "interweave";
 import { useEffect,useState } from "react";
 import { Routes, Route, useParams } from "react-router-dom";
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import BookmarkIcon from '@material-ui/icons/Bookmark';
 
 function ShowBlog(){
     
@@ -13,9 +15,52 @@ function ShowBlog(){
        frontImage:'',
        body:''
     });
-
-
+    
     const {blogID}=useParams();
+
+    const [isSave,setIsSave]=useState(false);
+    
+    const HandleSaveBlog=async()=>{
+        setIsSave(true);
+        const url='http://localhost:3000/blog/save';
+        
+        try {
+            const {data}=await axios({
+                method:'PUT',
+                url:url,
+                withCredentials:true,
+                data:{
+                    blogID:blogID
+                }
+            });
+
+        } catch (error) {
+            alert('something went wrong...');
+            console.log(error);
+        }
+    }
+    
+    const HanldeUnsaveBlog=async()=>{
+        
+        setIsSave(false);
+        const url='http://localhost:3000/blog/unSave';
+
+        try {
+            const {data}=await axios({
+                method:'PUT',
+                url:url,
+                withCredentials:true,
+                data:{
+                    blogID:blogID
+                }
+            })
+    
+        } catch (error) {
+            alert('something went wrong');
+        }
+    }
+
+    
     
     useEffect(()=>{
  
@@ -51,12 +96,51 @@ function ShowBlog(){
 
     },[blogID]);
 
+    useEffect(()=>{
+        
+        const url='http://localhost:3000/blog/isSave';
+        let source=axios.CancelToken.source();
+
+        const checkBlogSave=async()=>{
+            try {
+                const {data}=await axios({
+                    method:'POST',
+                    url:url,
+                    withCredentials:true,
+                    data:{
+                        blogID:blogID
+                    },
+                    cancelToken:source.token
+                });
+
+                setIsSave(data.isSave);
+            } catch (error) {
+                
+                if( axios.isCancel(error) ){
+
+                }else
+                   alert('something went wrong...');
+            }
+        }
+
+        checkBlogSave()
+     
+        return()=>{
+            source.cancel();
+        }
+
+    },[blogID]);
+
     return(
         <div className='show-blog-container'>
             <div className='show-blog'>
                 <div className='header-section'>
   
-                    <div className='blog-title'> {data.title} </div>
+                    <div className='blog-title-container'> 
+                        <div className='blog-title'>{data.title}</div>
+                        {isSave===true ? <BookmarkIcon className='bookmark-icon' onClick={HanldeUnsaveBlog}/> :<BookmarkBorderIcon className='bookmark-icon' onClick={HandleSaveBlog}/>}
+                    
+                    </div>
                     <div className='author-details'>
                         <img 
                            className='author-photo'
